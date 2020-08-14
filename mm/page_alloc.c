9550a7ca1f2a1173c74b7664eb6a33a0f429ea28
Ratoriku <a1063021545@gmail.com>
2020-08-14 20:38:58 +0800

    Revert "mm: Boost when memory pressure becomes high"
    This reverts commit 412e32162f3207a1f82de3798b17157a74dbbdb6.

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -62,8 +62,6 @@
 #include <linux/sched/rt.h>
 #include <linux/page_owner.h>
 #include <linux/kthread.h>
-#include <linux/cpu_input_boost.h>
-#include <linux/devfreq_boost.h>
 
 #include <asm/sections.h>
 #include <asm/tlbflush.h>
@@ -3274,11 +3272,6 @@ retry:
 		if (contended_compaction == COMPACT_CONTENDED_SCHED
 			&& !(current->flags & PF_KTHREAD))
 			goto nopage;
-
-	/* Boost when memory is low so allocation latency doesn't get too bad */
-	cpu_input_boost_kick_max(100);
-	devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 100);
-
 	}
 
 	/*
